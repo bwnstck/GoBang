79c8058f9238024306f00205a4374dcb88bcbbf0
bwnstk <info@flimmerkiste.xyz>
2020-10-27 11:55:35 +0100

    finish tutorial

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,30 +1,10 @@
 import "./board.css";
-import React, { useState } from "react";
+import React from "react";
 import Square from "./Square";
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(16).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const newSquares = squares.slice();
-    newSquares[i] = xIsNext ? "🦩" : "🦖";
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
-
+export default function Board(props) {
   function renderSquare(i) {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
-  }
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = `Next player:${xIsNext ? "🦩" : "🦖"}`;
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
   }
 
   return (
@@ -54,44 +34,6 @@ export default function Board() {
         {renderSquare(14)}
         {renderSquare(15)}
       </div>
-      <button
-        className="resetButton"
-        onClick={() => {
-          setSquares(Array(9).fill(null));
-        }}
-      >
-        Reset
-      </button>
     </div>
   );
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    // Reihenwin
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
-    // Zeilenwin
-    [0, 4, 8, 12],
-    [1, 5, 9, 13],
-    [2, 6, 10, 14],
-    [3, 7, 11, 15],
-    // Diagonalwin
-    [0, 5, 10, 15],
-    [3, 6, 9, 12],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c, d] = lines[i];
-    if (
-      squares[a] &&
-      squares[a] === squares[b] &&
-      squares[a] === squares[c] &&
-      squares[a] === squares[d]
-    ) {
-      return squares[a];
-    }
-  }
-  return null;
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,98 @@
 import "./game.css";
-import React from "react";
+import React, { useState } from "react";
 import Board from "./Board";
 
 export default function Game() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    const currentHistory = history.slice(0, stepNumber + 1);
+    const currentStep = currentHistory[currentHistory.length - 1];
+
+    const squares = currentStep.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "🦩" : "🦖";
+
+    setHistory(currentHistory.concat([{ squares: squares }]));
+    setStepNumber(currentHistory.length);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(step) {
+    console.log("jump to ", step);
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  }
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start: ";
+    return (
+      <li key={move}>
+        <button
+          onClick={() => {
+            jumpTo(move);
+            console.log(move, desc);
+          }}
+        >
+          {" "}
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = `Next player:${xIsNext ? "🦩" : "🦖"}`;
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    // Reihenwin
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [8, 9, 10, 11],
+    [12, 13, 14, 15],
+    // Zeilenwin
+    [0, 4, 8, 12],
+    [1, 5, 9, 13],
+    [2, 6, 10, 14],
+    [3, 7, 11, 15],
+    // Diagonalwin
+    [0, 5, 10, 15],
+    [3, 6, 9, 12],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c, d] = lines[i];
+    if (
+      squares[a] &&
+      squares[a] === squares[b] &&
+      squares[a] === squares[c] &&
+      squares[a] === squares[d]
+    ) {
+      return squares[a];
+    }
+  }
+  return null;
 }
