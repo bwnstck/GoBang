4fb7dc5c66faa55f9460b65fdfe97cd86e419829
bwnstk <info@flimmerkiste.xyz>
2020-11-02 19:59:50 +0100

    now with onClick

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,7 +1,6 @@
 import "./board.css";
 import React from "react";
 import Square from "./Square";
-import { render } from "react-dom";
 
 export default function Board(props) {
   const squares = Array(64).fill(null);
@@ -10,7 +9,11 @@ export default function Board(props) {
     <div>
       <div className="status">{status}</div>
       {squares.map((square, index) => (
-        <Square key={index} value={square} />
+        <Square
+          key={index}
+          value={square}
+          onClick={() => props.onClick(square)}
+        />
       ))}
     </div>
   );
